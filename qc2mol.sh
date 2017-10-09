d2ee6472b324fbf611b110b57b8037c4dbf28059
Jan Mewes (humboldt <mewes@ctcp-cluster.cm.cluster>
2017-10-09 15:58:05 +1300

    update

--- qc2mol.sh
+++ qc2mol.sh
@@ -6,7 +6,11 @@ for i in *out
 	do if [ $(grep -o "Z-matrix" $i) ] 
 	then 
 		molfile=$(echo $i | sed 's/.out/.mol/')
-        natoms=$(grep "Nuclear Repulsion Energy =  " -b2 $i | head -n 1 | awk '{print $2}')
+        if [ $(grep "Nuclear Repulsion Energy =  " -b2 $i | head -n 1 | awk '{print $2}') = "Molecular" ]
+		then echo "Symmetry detected" 
+		natoms=$(grep "Nuclear Repulsion Energy =  " -b4 $i | head -n 1 | awk '{print $2}')
+		else natoms=$(grep "Nuclear Repulsion Energy =  " -b2 $i | head -n 1 | awk '{print $2}')
+	fi
         headlines=$((natoms+4))
 		taillines=$((natoms+3))
 		grep "Z-matrix" $i -A1000 | head -n $headlines | tail -n $taillines > $molfile
--- sort_plots_adc.sh
+++ sort_plots_adc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mult="adc"
+
 mkdir MOs MP2 STATE_DENS TRANS_DENS
 
 mv mo.* MOs
@@ -7,28 +9,54 @@ mv *mp2* MP2
 mv *dens.cube STATE_DENS
 mv *trans.cube TRANS_DENS
 
-for i in *_attach.cube 
-	do j=$(echo $i | sed s/attach/4attach/)
+for i in singlet*_attach.cube 
+	do j=$(echo $i | sed "s/attach/sing_2att/" | sed s/singlet_//)
 	mv $i $j 
 done
 
-for i in *_detach.cube 
-	do j=$(echo $i | sed s/detach/2detach/)
+for i in singlet*_detach.cube 
+	do j=$(echo $i | sed "s/detach/sing_4det/"| sed s/singlet_//)
 	mv $i $j 
 done
 
-for i in *_elec.cube 
-	do j=$(echo $i | sed s/elec/3elec/)
+for i in singlet*_diff.cube 
+	do j=$(echo $i | sed "s/diff/sing_5dif/"| sed s/singlet_//)
 	mv $i $j 
 done
 
-for i in *_hole.cube 
-	do j=$(echo $i | sed s/hole/1hole/)
+for i in singlet*_elec.cube 
+	do j=$(echo $i | sed "s/elec/sing_3ele/"| sed s/singlet_//)
 	mv $i $j 
 done
 
-for i in *_diff.cube 
-	do j=$(echo $i | sed s/diff/5diff/)
+for i in singlet*_hole.cube 
+	do j=$(echo $i | sed "s/hole/sing_1hol/"| sed s/singlet_//)
 	mv $i $j 
 done
+
+for i in triplet*_attach.cube 
+	do j=$(echo $i | sed "s/attach/trip_2att/"| sed s/triplet_//)
+	mv $i $j 
+done
+
+for i in triplet*_detach.cube 
+	do j=$(echo $i | sed "s/detach/trip_4det/"| sed s/triplet_//)
+	mv $i $j 
+done
+
+for i in triplet*_diff.cube 
+	do j=$(echo $i | sed "s/diff/trip_5dif/"| sed s/triplet_//)
+	mv $i $j 
+done
+
+for i in triplet*_elec.cube 
+	do j=$(echo $i | sed "s/elec/trip_3ele/"| sed s/triplet_//)
+	mv $i $j 
+done
+
+for i in triplet*_hole.cube 
+	do j=$(echo $i | sed "s/hole/trip_1hol/"| sed s/triplet_//)
+	mv $i $j 
+done
+
 
